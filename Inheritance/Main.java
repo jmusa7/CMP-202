cbccdfdeb243d81b147c7e4b3855dbfb2cc5f7c6
Jason Musa Msheliza <134550990+jmusa7@users.noreply.github.com>
2024-06-24 09:01:15 +0100

    Add files via upload
    Jason Musa
    BHU/20/04/09/0137

---
+++ Inheritance/Main.java
@@ -0,0 +1,8 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        CMP202 printUser = new CMP202();
+        printUser.printUserDetails();
+    }
+}
---
+++ Inheritance/User.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class User {
+    String username = "Dan-Codes";
+    String email = "daniel@mail.com";
+    String login() {
+        if (username.equals("Dan-Codes") && email.equals("daniel@mail.com")) {
+            return "Logged in";
+        } else {
+            return "Failed";
+        }
+    }
+    public static void main(String[] args) {
+        User jane = new User();
+        System.out.println(jane.login());
+    }
+}
