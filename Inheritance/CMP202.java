5642ef6726d22588ab587f4bc29c92ff3c39b629
Jason Musa Msheliza <134550990+jmusa7@users.noreply.github.com>
2024-07-06 12:08:58 +0100

    Update CMP202.java

--- Inheritance/CMP202.java
+++ Inheritance/CMP202.java
@@ -8,12 +8,12 @@ public class CMP202 {
 
     void printUserDetails() {
         age = 23;
-        height = 172;
-        name = "Dan-Codes";
+        height = 177.8F;
+        name = "Sad-Jason";
         name.length();
         password = "User@123";
-        if (password.contains("123") || password.contains("User")) {
-            System.out.println("Password is not strong enough");
+        if (password.contains("12345") || password.contains("User")) {
+            System.out.println("The password meets strength requirements.");
         }
     }    
 }
